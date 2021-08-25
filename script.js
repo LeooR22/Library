04855811af40855360bf1be9235a16a4f13bccb2
leoor22 <leonelrivas.rl@gmail.com>
2021-08-25 17:23:29 -0300

    Progress in HTML, CSS & JS

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-function book(title, author, pages, read) {
+let myLibrary = [];
+
+// Funcion constructor
+
+function book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    this.info = function() {
+    this.readStatus = readStatus;
+    /* this.info = function() {
         console.log(title + " by " + author + ", " + pages + " pages, " 
                             + read);
-    }
+    } */
 }
 
-const theHobbit = new book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
+// default's books
+
+theHobbit = new book("The Hobbit", "J.R.R. Tolkien", 295, "Unread")
+myLibrary.push(theHobbit)
+
+donQuixote = new book("Don Quixote", "Miguel de Cervantes", 1345, "Unread")
+myLibrary.push(donQuixote)
+
+theLittlePrince = new book("The Little Prince", "Antoine de Saint-Exupery", 96, "Read")
+myLibrary.push(theLittlePrince)
+
+//
+
+function addBookToLibrary() {
+    myLibrary.push(new book(prompt("Title"), prompt("Author"), prompt("Pages"),
+                             prompt("readStatus")))
+
+}
