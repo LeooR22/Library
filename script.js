997a230b4cd3349c7ca2909c35eb0662cf21f886
leoor22 <leonelrivas.rl@gmail.com>
2021-08-25 23:51:45 -0300

    Progres in script

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let myLibrary = [];
 
-// Funcion constructor
+const txtTitle = document.getElementById("title")
+const txtAuthor = document.getElementById("author")
+const txtPages = document.getElementById("pages")
+const txtReadStatus = document.getElementById("readStatus")
 
+const btnSubmit = document.getElementById("submit")
+
+btnSubmit.addEventListener("click", uploadBook)
+
+
+// Funcion constructor
 function book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
@@ -12,8 +21,21 @@ function book(title, author, pages, readStatus) {
                             + read);
     } */
 }
+//
 
-// default's books
+// Funcion Agregar libro a myLibrary
+function uploadBook(){
+    newBook = new book(txtTitle.value, txtAuthor.value, txtPages.value, txtReadStatus.value)
+    myLibrary.push(newBook);
+    txtTitle.value = "";
+    txtAuthor.value = "";
+    txtPages.value = "";
+    txtReadStatus.value = "";   
+}
+//
+
+//  carga de defaultBooks
+function defaultBooks(){
 
 theHobbit = new book("The Hobbit", "J.R.R. Tolkien", 295, "Unread")
 myLibrary.push(theHobbit)
@@ -23,11 +45,7 @@ myLibrary.push(donQuixote)
 
 theLittlePrince = new book("The Little Prince", "Antoine de Saint-Exupery", 96, "Read")
 myLibrary.push(theLittlePrince)
-
-//
-
-function addBookToLibrary() {
-    myLibrary.push(new book(prompt("Title"), prompt("Author"), prompt("Pages"),
-                             prompt("readStatus")))
-
 }
+defaultBooks()
+//
+    
