38517a4349e7d60b40e299e820665905f7701184
leoor22 <leonelrivas.rl@gmail.com>
2021-08-28 13:23:41 -0300

    Progress in css and js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 let myLibrary = [];
 
+//vinculacion con inputs html
 const txtTitle = document.getElementById("title")
 const txtAuthor = document.getElementById("author")
 const txtPages = document.getElementById("pages")
 const txtReadStatus = document.getElementById("readStatus")
-
+//
 const btnSubmit = document.getElementById("submit")
 
 btnSubmit.addEventListener("click", uploadBook)
 
 
 // Funcion constructor
-function book(title, author, pages, readStatus) {
+function book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.readStatus = readStatus;
+    this.status = status;
     /* this.info = function() {
         console.log(title + " by " + author + ", " + pages + " pages, " 
                             + read);
@@ -30,7 +31,9 @@ function uploadBook(){
     txtTitle.value = "";
     txtAuthor.value = "";
     txtPages.value = "";
-    txtReadStatus.value = "";   
+    txtReadStatus.value = "";
+    document.getElementById("myLibraryTable").innerHTML = null
+    buildTable(topTable, myLibrary, document.getElementById('myLibraryTable'));
 }
 //
 
@@ -48,4 +51,41 @@ myLibrary.push(theLittlePrince)
 }
 defaultBooks()
 //
-    
+
+// Creation of myLibrary table
+
+var topTable = ["Title", "Author", "Pages", "Status"];
+
+// Estructure of object table
+function buildTable(labels, objects, container) {
+    var table = document.createElement('table');
+    var thead = document.createElement('thead');
+    var tbody = document.createElement('tbody');
+  
+    var theadTr = document.createElement('tr');
+    for (var i = 0; i < labels.length; i++) {
+      var theadTh = document.createElement('th');
+      theadTh.innerHTML = labels[i];
+      theadTr.appendChild(theadTh);
+    }
+    thead.appendChild(theadTr);
+    table.appendChild(thead);
+  
+    for (j = 0; j < objects.length; j++) {
+      var tbodyTr = document.createElement('tr');
+      for (k = 0; k < labels.length; k++) {
+        var tbodyTd = document.createElement('td');
+        tbodyTd.innerHTML = objects[j][labels[k].toLowerCase()];
+        tbodyTr.appendChild(tbodyTd);
+      }
+      tbody.appendChild(tbodyTr);
+    }
+    table.appendChild(tbody);
+  
+    container.appendChild(table);
+  }
+
+//
+
+  buildTable(topTable, myLibrary, document.getElementById('myLibraryTable'));
+
